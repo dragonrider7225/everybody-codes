4131025fa73050eb75672dafc57b47da652ac7f2
Kevin Moonen <dragonrider7225@gmail.com>
2024-12-28 21:05:57 -0600

    Implement algorithmia-01

--- algorithmia/src/quest_1/mod.rs
+++ algorithmia/src/quest_1/mod.rs
@@ -3,16 +3,81 @@ use std::{
     io::{self, BufRead, BufReader},
 };
 
-fn part1(input: &mut dyn BufRead) -> io::Result<u32> {
-    todo!("Algorithmia Quest 1 Part 1")
+enum Enemy {
+    A,
+    B,
+    C,
+    D,
 }
 
-fn part2(input: &mut dyn BufRead) -> io::Result<u32> {
-    todo!("Algorithmia Quest 1 Part 2")
+impl Enemy {
+    fn potion_cost(&self) -> usize {
+        match self {
+            Self::A => 0,
+            Self::B => 1,
+            Self::C => 3,
+            Self::D => 5,
+        }
+    }
 }
 
-fn part3(input: &mut dyn BufRead) -> io::Result<u32> {
-    todo!("Algorithmia Quest 1 Part 3")
+impl TryFrom<u8> for Enemy {
+    type Error = ();
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            b'A' => Ok(Self::A),
+            b'B' => Ok(Self::B),
+            b'C' => Ok(Self::C),
+            b'D' => Ok(Self::D),
+            _ => Err(()),
+        }
+    }
+}
+
+fn part1(input: &mut dyn BufRead) -> io::Result<usize> {
+    let input = io::read_to_string(input)?;
+    Ok(input
+        .trim()
+        .bytes()
+        .map(|b| {
+            Enemy::try_from(b)
+                .expect("Invalid enemy: {b:?}")
+                .potion_cost()
+        })
+        .sum())
+}
+
+fn part2(input: &mut dyn BufRead) -> io::Result<usize> {
+    let input = io::read_to_string(input)?;
+    let input = input.trim().as_bytes();
+    Ok(input
+        .chunks(2)
+        .map(|chunk| [Enemy::try_from(chunk[0]), Enemy::try_from(chunk[1])])
+        .map(|pair| match pair {
+            [Ok(enemy), Err(_)] | [Err(_), Ok(enemy)] => enemy.potion_cost(),
+            [Ok(enemy1), Ok(enemy2)] => enemy1.potion_cost() + enemy2.potion_cost() + 2,
+            [Err(_), Err(_)] => 0,
+        })
+        .sum())
+}
+
+fn part3(input: &mut dyn BufRead) -> io::Result<usize> {
+    let input = io::read_to_string(input)?;
+    let input = input.trim().as_bytes();
+    Ok(input
+        .chunks(3)
+        .map(|chunk| {
+            chunk
+                .iter()
+                .filter_map(|&e| Enemy::try_from(e).ok())
+                .fold((0, 0), |(count, cost), enemy| {
+                    (count + 1, cost + enemy.potion_cost())
+                })
+        })
+        .filter(|&(count, _)| count > 0)
+        .map(|(count, cost)| count * (count - 1) + cost)
+        .sum())
 }
 
 pub(super) fn run() -> io::Result<()> {
@@ -38,4 +103,35 @@ pub(super) fn run() -> io::Result<()> {
         );
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use io::Cursor;
+
+    use super::*;
+
+    #[test]
+    fn test_part1() -> io::Result<()> {
+        let expected = 5;
+        let actual = part1(&mut Cursor::new("ABBAC\n"))?;
+        assert_eq!(expected, actual);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> io::Result<()> {
+        let expected = 28;
+        let actual = part2(&mut Cursor::new("AxBCDDCAxD\n"))?;
+        assert_eq!(expected, actual);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part3() -> io::Result<()> {
+        let expected = 30;
+        let actual = part3(&mut Cursor::new("xBxAAABCDxCC\n"))?;
+        assert_eq!(expected, actual);
+        Ok(())
+    }
 }
