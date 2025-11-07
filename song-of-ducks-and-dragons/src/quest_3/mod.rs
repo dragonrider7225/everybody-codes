cbd69cea5ae8af728361579affea9a8370013524
Kevin Moonen <dragonrider7225@gmail.com>
2025-11-06 18:41:56 -0600

    Implement The Song of Ducks and Dragons Quest 3

--- song-of-ducks-and-dragons/src/quest_3/mod.rs
+++ song-of-ducks-and-dragons/src/quest_3/mod.rs
@@ -3,16 +3,72 @@ use std::{
     io::{self, BufRead, BufReader},
 };
 
-fn part1(_input: &mut dyn BufRead) -> io::Result<u32> {
-    todo!("Song of Ducks and Dragons Quest 3 Part 1")
+fn part1(input: &mut dyn BufRead) -> io::Result<usize> {
+    let mut crates = input
+        .lines()
+        .next()
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Couldn't read crate sizes"))??
+        .split(',')
+        .map(|s| {
+            s.parse::<usize>().map_err(|_| {
+                io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    format!("Couldn't parse {s:?} as crate size"),
+                )
+            })
+        })
+        .collect::<io::Result<Vec<_>>>()?;
+    crates.sort();
+    crates.dedup();
+    Ok(crates.into_iter().sum::<usize>())
 }
 
-fn part2(_input: &mut dyn BufRead) -> io::Result<u32> {
-    todo!("Song of Ducks and Dragons Quest 3 Part 2")
+fn part2(input: &mut dyn BufRead) -> io::Result<usize> {
+    let mut crates = input
+        .lines()
+        .next()
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Couldn't read crate sizes"))??
+        .split(',')
+        .map(|s| {
+            s.parse::<usize>().map_err(|_| {
+                io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    format!("Couldn't parse {s:?} as crate size"),
+                )
+            })
+        })
+        .collect::<io::Result<Vec<_>>>()?;
+    crates.sort();
+    crates.dedup();
+    Ok(crates.into_iter().take(20).sum::<usize>())
 }
 
-fn part3(_input: &mut dyn BufRead) -> io::Result<u32> {
-    todo!("Song of Ducks and Dragons Quest 3 Part 3")
+fn part3(input: &mut dyn BufRead) -> io::Result<usize> {
+    let mut crates = input
+        .lines()
+        .next()
+        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Couldn't read crate sizes"))??
+        .split(',')
+        .map(|s| {
+            s.parse::<usize>().map_err(|_| {
+                io::Error::new(
+                    io::ErrorKind::InvalidData,
+                    format!("Couldn't parse {s:?} as crate size"),
+                )
+            })
+        })
+        .collect::<io::Result<Vec<_>>>()?;
+    crates.sort();
+    let mut max_freq = 0;
+    let mut curr_start = 0;
+    for i in 0..crates.len() {
+        if crates[i] != crates[curr_start] {
+            let curr_freq = i - curr_start;
+            max_freq = max_freq.max(curr_freq);
+            curr_start = i;
+        }
+    }
+    Ok(max_freq.max(crates.len() - curr_start))
 }
 
 pub(super) fn run() -> io::Result<()> {
@@ -44,4 +100,38 @@ pub(super) fn run() -> io::Result<()> {
         );
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Cursor;
+
+    use super::*;
+
+    #[test]
+    fn test_part1() -> io::Result<()> {
+        const TEST_DATA: &str = "10,5,1,10,3,8,5,2,2\n";
+        let expected = 29;
+        let actual = part1(&mut Cursor::new(TEST_DATA))?;
+        assert_eq!(expected, actual);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> io::Result<()> {
+        const TEST_DATA: &str = "4,51,13,64,57,51,82,57,16,88,89,48,32,49,49,2,84,65,49,43,9,13,2,3,75,72,63,48,61,14,40,77\n";
+        let expected = 781;
+        let actual = part2(&mut Cursor::new(TEST_DATA))?;
+        assert_eq!(expected, actual);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part3() -> io::Result<()> {
+        const TEST_DATA: &str = "4,51,13,64,57,51,82,57,16,88,89,48,32,49,49,2,84,65,49,43,9,13,2,3,75,72,63,48,61,14,40,77\n";
+        let expected = 3;
+        let actual = part3(&mut Cursor::new(TEST_DATA))?;
+        assert_eq!(expected, actual);
+        Ok(())
+    }
 }
